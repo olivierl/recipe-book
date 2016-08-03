adc7000316f33b8dbb96d5f16b46b8d678913ca3
olivierl <olivier.lambeaux@gmail.com>
2016-08-03 22:54:41 +0200

    add recipes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  directives: [HeaderComponent]
+  directives: [HeaderComponent, RecipesComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { RecipeDetailComponent } from './recipe-detail.component';
+
+describe('Component: RecipeDetail', () => {
+  it('should create an instance', () => {
+    let component = new RecipeDetailComponent();
+    expect(component).toBeTruthy();
+  });
+});
