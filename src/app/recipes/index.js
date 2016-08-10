7f745165d0bc1610ba2d8f0a913671826c66aa78
olivierl <olivier.lambeaux@gmail.com>
2016-08-10 10:55:55 +0200

    remove all .js and .map files from src

--- src/app/recipes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var recipes_component_1 = require('./recipes.component');
-exports.RecipesComponent = recipes_component_1.RecipesComponent;
-var recipe_service_1 = require('./recipe.service');
-exports.RecipeService = recipe_service_1.RecipeService;
-var recipes_routes_1 = require('./recipes.routes');
-exports.RECIPES_ROUTES = recipes_routes_1.RECIPES_ROUTES;
-//# sourceMappingURL=index.js.map
